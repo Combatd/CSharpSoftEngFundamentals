bcd5ffef8f73e1a8b2d99bedc2b63cfd4c539a29
Combatd <combatdstar@gmail.com>
2021-02-26 14:07:03 -0800

    Write comments

--- ExercisesPartOne/ExercisesPartOne/Program.cs
+++ ExercisesPartOne/ExercisesPartOne/Program.cs
@@ -7,6 +7,24 @@ namespace ExercisesPartOne
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Comments
+            // Single Line Comment
+            /* 
+             Multi Line Comment
+             */
+            // **********
+            /// <summary>
+            ///
+            /// </summary>
+            ///
+            /// <example>
+            /// XML Comment
+            /// </example>
+            // **********
+
+
+            
         }
     }
 }

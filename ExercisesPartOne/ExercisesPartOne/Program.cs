800fc87c90e2b0bced98dac7003e15db92859841
Combatd <combatdstar@gmail.com>
2021-02-27 15:10:22 -0800

    Search and Split strings

--- ExercisesPartOne/ExercisesPartOne/Program.cs
+++ ExercisesPartOne/ExercisesPartOne/Program.cs
@@ -24,103 +24,115 @@ namespace ExercisesPartOne
             // **********
 
             // Variables
-            bool exerciseBool = true;
-            double exerciseDouble = 1.25;
-            char exerciseChar = 'A';
-            string exerciseString = "Gawr Gura";
+            //bool exerciseBool = true;
+            //double exerciseDouble = 1.25;
+            //char exerciseChar = 'A';
+            //string exerciseString = "Gawr Gura";
 
-            Console.WriteLine($"{exerciseBool} {exerciseDouble} {exerciseChar} {exerciseString}");
+            //Console.WriteLine($"{exerciseBool} {exerciseDouble} {exerciseChar} {exerciseString}");
 
-            // Min / Max values
+            //// Min / Max values
 
-            //int maxInt = int.MaxValue;
-            //Console.WriteLine(maxInt);
-            //maxInt++; // increment maxValue by 1
-            //Console.WriteLine(maxInt); // goes back to lowest number minimum int
+            ////int maxInt = int.MaxValue;
+            ////Console.WriteLine(maxInt);
+            ////maxInt++; // increment maxValue by 1
+            ////Console.WriteLine(maxInt); // goes back to lowest number minimum int
 
-            //// Implicit Types
-            //var newBool = false;
-            //var newInt = 12;
-            //var newChar = 'C';
-            //var newString = "This should resolve to a string type";
-            //Console.WriteLine(newChar + newInt + newString);
-            //Console.WriteLine(newBool);
+            ////// Implicit Types
+            ////var newBool = false;
+            ////var newInt = 12;
+            ////var newChar = 'C';
+            ////var newString = "This should resolve to a string type";
+            ////Console.WriteLine(newChar + newInt + newString);
+            ////Console.WriteLine(newBool);
 
-            //// Operators
+            ////// Operators
+            //////int x = 1;
+            //////Console.WriteLine(x);
+            //////int y = 2;
+            //////Console.WriteLine(y);
+            //////int z1 = x++;
+            //////Console.WriteLine(z1);
+            //////int z2 = ++y;
+            //////Console.WriteLine(z2);
+
+            ////// Math
             ////int x = 1;
-            ////Console.WriteLine(x);
             ////int y = 2;
-            ////Console.WriteLine(y);
-            ////int z1 = x++;
-            ////Console.WriteLine(z1);
-            ////int z2 = ++y;
-            ////Console.WriteLine(z2);
+            ////int z = 3;
 
-            //// Math
+            ////int a = x + y;
+            ////int b = y - x;
+            ////int c = z / x + y;
+
+            ////int notEven = 3 % 2;
+            ////bool isExpressionEven = notEven == 0;
+
+            ////double threeDecimalDouble = Math.Round(10.23456 / 10, 3);
+
+            ////Console.WriteLine(isExpressionEven);
+            ////Console.WriteLine(threeDecimalDouble);
+
+            //// Type Conversions
             //int x = 1;
             //int y = 2;
             //int z = 3;
 
-            //int a = x + y;
-            //int b = y - x;
-            //int c = z / x + y;
+            //double a = 1.2;
+            //double b = 2.2;
+            //double c = 3.2;
 
-            //int notEven = 3 % 2;
-            //bool isExpressionEven = notEven == 0;
+            //int oldX = x;
+            //int oldY = y;
+            //int oldZ = z;
 
-            //double threeDecimalDouble = Math.Round(10.23456 / 10, 3);
+            //x = (int)a;
+            //y = (int)b;
+            //z = (int)c;
 
-            //Console.WriteLine(isExpressionEven);
-            //Console.WriteLine(threeDecimalDouble);
+            //a = (double)oldX;
+            //b = (double)oldY;
+            //c = (double)oldZ;
 
-            // Type Conversions
-            int x = 1;
-            int y = 2;
-            int z = 3;
+            //Console.WriteLine(x); // becomes int
+            //Console.WriteLine(a); // becomes double with no decimal
 
-            double a = 1.2;
-            double b = 2.2;
-            double c = 3.2;
+            //// Strings
 
-            int oldX = x;
-            int oldY = y;
-            int oldZ = z;
+            //string escapeSeqStr = "Hello..\\ \n ..World!";
+            //Console.WriteLine(escapeSeqStr);
+            //string literalStr = $"This is a string literal - {escapeSeqStr}";
+            //Console.WriteLine(literalStr);
+            //Console.WriteLine(escapeSeqStr + $"length is {escapeSeqStr.Length}");
 
-            x = (int)a;
-            y = (int)b;
-            z = (int)c;
+            //// Index
+            //string burger = "Burger!";
+            //Console.WriteLine(burger[0].ToString() + burger[burger.Length / 2].ToString() + burger[burger.Length - 1].ToString()); // used ToString to get strings instead of index numbers
 
-            a = (double)oldX;
-            b = (double)oldY;
-            c = (double)oldZ;
+            //// Changing case
+            //string capsTestStr = "Characters";
+            //Console.WriteLine(capsTestStr);
+            //Console.WriteLine(capsTestStr.ToLower());
+            //Console.WriteLine(capsTestStr.ToUpper());
 
-            Console.WriteLine(x); // becomes int
-            Console.WriteLine(a); // becomes double with no decimal
+            //// Getting parts of strings
+            //string partingStr = "Senzawa";
+            //partingStr = partingStr.Remove(0, 1);
+            //partingStr = partingStr.Remove(partingStr.Length - 1, 1);
+            //Console.WriteLine(partingStr);
 
-            // Strings
+            // Searching
+            string x = "Korone";
+            char y = 'o';
 
-            string escapeSeqStr = "Hello..\\ \n ..World!";
-            Console.WriteLine(escapeSeqStr);
-            string literalStr = $"This is a string literal - {escapeSeqStr}";
-            Console.WriteLine(literalStr);
-            Console.WriteLine(escapeSeqStr + $"length is {escapeSeqStr.Length}");
+            int foundChar = x.IndexOf(y);
+            bool charInStr = x.Contains(y);
 
-            // Index
-            string burger = "Burger!";
-            Console.WriteLine(burger[0].ToString() + burger[burger.Length / 2].ToString() + burger[burger.Length - 1].ToString()); // used ToString to get strings instead of index numbers
+            string fakeFilePath = "c:\\fake\\bar.txt";
+            string[] splitFilePath = fakeFilePath.Split('.');
+            string noExtension = splitFilePath[0];
 
-            // Changing case
-            string capsTestStr = "Characters";
-            Console.WriteLine(capsTestStr);
-            Console.WriteLine(capsTestStr.ToLower());
-            Console.WriteLine(capsTestStr.ToUpper());
-
-            // Getting parts of strings
-            string partingStr = "Senzawa";
-            partingStr = partingStr.Remove(0, 1);
-            partingStr = partingStr.Remove(partingStr.Length - 1, 1);
-            Console.WriteLine(partingStr);
-
+            Console.WriteLine(noExtension);
         }
     }
 }

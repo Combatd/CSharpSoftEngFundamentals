33f585c04d4508ed6ce657df82516543c3c2d0b0
Combatd <combatdstar@gmail.com>
2021-02-27 18:53:20 -0800

    Create ExercisesPartTwo namespace

---
+++ ExercisesPartTwo/ExercisesPartTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercisesPartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

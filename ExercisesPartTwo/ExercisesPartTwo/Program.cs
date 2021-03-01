4a207d20f81ae109581e3865f3486dd71add19ab
Combatd <combatdstar@gmail.com>
2021-02-28 18:21:02 -0800

    Check x value in conditionals

--- ExercisesPartTwo/ExercisesPartTwo/Program.cs
+++ ExercisesPartTwo/ExercisesPartTwo/Program.cs
@@ -6,7 +6,20 @@ namespace ExercisesPartTwo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Conditionals
+
+            // If/else
+            int x = 1;
+            if (x < 100)
+            {
+                Console.WriteLine(x + " is less than 100");
+            } else if(x == 100)
+            {
+                Console.WriteLine($"{x} is 100!");
+            } else {
+                Console.WriteLine("The x is not a non-negative number 100 or less");
+            }
+
         }
     }
 }
